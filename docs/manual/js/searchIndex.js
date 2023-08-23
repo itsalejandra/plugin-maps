0f47e7b6c851505bb4ea542e99e752c7a277d150
Alejandra Sanchez <alejandravsanchezlorenzo@gmail.com>
2023-08-23 19:29:31 +0200

    1

---
+++ docs/manual/js/searchIndex.js
@@ -0,0 +1,14 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\map_shortcode\u0028\u0029",
+            "name": "map_shortcode",
+            "summary": "Plugin\u0020Mapas",
+            "url": "namespaces/default.html#function_map_shortcode"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
